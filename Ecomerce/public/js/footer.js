cf266d0106549f3b7915a8bfe93b5e5c5dc9d914
Leart Bajrami <73801402+Leart-C@users.noreply.github.com>
2021-09-19 23:02:52 +0200

    Add files via upload

---
+++ Ecomerce/public/js/footer.js
@@ -0,0 +1,68 @@
+const createFooter = () =>{
+    let footer  = document.querySelector('footer');
+
+    footer.innerHTML=`
+    <div class="footer-content">
+    <img src="img/light-logo.png" class="logo" alt="">
+    <div class="footer-ul-container">
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li><a href="#" class="footer-link">t-shirts</a></li>
+            <li><a href="#" class="footer-link">sweatshirts</a></li>
+            <li><a href="#" class="footer-link">shirts</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+        <ul class="category">
+            <li class="category-title">men</li>
+            <li><a href="#" class="footer-link">t-shirts</a></li>
+            <li><a href="#" class="footer-link">sweatshirts</a></li>
+            <li><a href="#" class="footer-link">shirts</a></li>
+            <li><a href="#" class="footer-link">jeans</a></li>
+            <li><a href="#" class="footer-link">trousers</a></li>
+            <li><a href="#" class="footer-link">shoes</a></li>
+            <li><a href="#" class="footer-link">casuals</a></li>
+            <li><a href="#" class="footer-link">formals</a></li>
+            <li><a href="#" class="footer-link">sports</a></li>
+            <li><a href="#" class="footer-link">watch</a></li>
+        </ul>
+    </div>
+
+</div>
+<p class="footer-title">about company</p>
+<p class="info">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, eaque? Dolore sapiente
+    dolorum fugiat debitis deleniti, quia ipsa, quaerat soluta quod magni necessitatibus. Eos debitis natus
+    reprehenderit, voluptatibus cupiditate fuga mollitia, officiis labore veniam voluptatem cum iste illum,
+    magni animi modi praesentium autem quod. Possimus eum dignissimos, quas nobis ipsam veritatis eaque fugit ex
+    porro voluptatum voluptatem ipsa obcaecati dolorem labore necessitatibus tempora repellendus sint qui
+    pariatur placeat laboriosam. Aliquid cupiditate excepturi reprehenderit eos ipsum minima facilis quae
+    aliquam repellendus, nulla temporibus natus, nemo nobis voluptatem maxime quisquam nam repudiandae culpa.
+    Sint iste minus voluptatum quas eligendi similique aspernatur illo.</p>
+<p class="info">support emails- help@clothing.com,
+    customersupport@clothing.com</p>
+<p class="info">telephone - 180 00 00 001, 180 00 00 002</p>
+
+<div class="footer-social-container">
+    <div>
+        <a href="#" class="social-link">terms & services</a>
+        <a href="#" class="socail-link0">privacy page</a>
+
+    </div>
+
+    <div>
+        <a href="#" class="socail-link2">facebook</a>
+        <a href="#" class="socail-link3">twitter</a>
+    </div>
+</div>
+
+<p class="footer-credit">
+    Clothing, Best apparels online store
+</p>
+    `;
+}
+createFooter();
